f579d605ce27b4049d4a06ea3e52545877062909
Kasey Hinton <kaseyjameshinton@gmail.com>
2018-09-26 16:28:13 -0600

    Adds effective updating when swapping between decks.

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -2,22 +2,41 @@ import React from "react";
 import { ScrollView, StyleSheet, Text, Button, View } from "react-native";
 
 export default class DeckScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      deck: {}
+    };
+  }
+
   static navigationOptions = {
     title: "Deck"
   };
 
+  componentDidMount() {
+    this.setState({ deck: this.props.navigation.getParam("deck") });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      JSON.stringify(this.state.deck) !==
+      JSON.stringify(this.props.navigation.getParam("deck"))
+    ) {
+      this.setState({ deck: this.props.navigation.getParam("deck") });
+    }
+  }
+
   render() {
     const { navigate } = this.props.navigation;
-    const DECK = navigation.getParam("deck");
 
     return (
       <ScrollView style={styles.container}>
-        <Text>{DECK.title}</Text>
+        <Text>{this.state.deck.title}</Text>
         <View style={{ marginTop: 16 }}>
           <Button
             onPress={() =>
               navigate("AddCard", {
-                deck: DECK,
+                deck: this.state.deck,
                 getDecks: this.props.navigation.state.params.getDecks
               })
             }
@@ -27,7 +46,7 @@ export default class DeckScreen extends React.Component {
         </View>
         <View style={{ marginTop: 16 }}>
           <Button
-            onPress={() => navigate("Quiz", { deck: DECK })}
+            onPress={() => navigate("Quiz", { deck: this.state.deck })}
             title="Start Quiz"
             color="#841584"
           />
