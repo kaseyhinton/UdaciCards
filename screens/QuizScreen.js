56f8971dd8327eaefa95ccf33edaba7336e09c72
Kasey Hinton <kaseyjameshinton@gmail.com>
2018-09-26 16:27:40 -0600

    Adds score screen to QuizScreen. Fixes updating when swapping between multiple quizzes. Adds initial state.

--- screens/QuizScreen.js
+++ screens/QuizScreen.js
@@ -1,10 +1,31 @@
 import React from "react";
-import { ScrollView, StyleSheet, Text, Button } from "react-native";
+import { ScrollView, StyleSheet, Text, Button, View } from "react-native";
 
 export default class QuizScreen extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      deck: {
+        questions: []
+      },
+      idx: 0,
+      correct: 0,
+      showAnswer: false,
+      finished: false
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ deck: this.props.navigation.getParam("deck") });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      JSON.stringify(this.state.deck) !==
+      JSON.stringify(this.props.navigation.getParam("deck"))
+    ) {
+      this.setState({ deck: this.props.navigation.getParam("deck") });
+    }
   }
 
   static navigationOptions = {
@@ -12,14 +33,132 @@ export default class QuizScreen extends React.Component {
   };
 
   render() {
-    const { navigation } = this.props;
-    const DECK = navigation.getParam("deck");
+    const deck = this.state.deck;
+    const idx = this.state.idx;
 
-    return (
-      <ScrollView style={styles.container}>
-        <Text>{DECK.title}</Text>
-      </ScrollView>
-    );
+    if (this.state.finished) {
+      return (
+        <ScrollView style={styles.container}>
+          <Text style={styles.mainText}>Quiz Complete</Text>
+          <Text style={styles.mainText}>
+            You Scored{" "}
+            {((this.state.correct / deck.questions.length) * 100).toFixed(1)} %
+          </Text>
+          <Text style={styles.subText}>
+            {this.state.correct} / {deck.questions.length}
+          </Text>
+          <View style={{ marginTop: 16 }}>
+            <Button
+              onPress={this._handleHomePress.bind(this)}
+              title="Home"
+              color="#841584"
+            />
+          </View>
+
+          <View style={{ marginTop: 16 }}>
+            <Button
+              onPress={this._handleTryAgainPress.bind(this)}
+              title="Try Again"
+              color="#841584"
+            />
+          </View>
+        </ScrollView>
+      );
+    }
+
+    if (deck && deck.questions && deck.questions.length > 0) {
+      return (
+        <ScrollView style={styles.container}>
+          <Text>{deck.title}</Text>
+          <Text>
+            {idx + 1} / {deck.questions.length}
+          </Text>
+
+          {this.state.showAnswer ? (
+            <View>
+              <Text style={styles.mainText}>{deck.questions[idx].answer}</Text>
+              <Text
+                style={styles.subText}
+                onPress={() => this.setState({ showAnswer: false })}
+              >
+                Show Question
+              </Text>
+            </View>
+          ) : (
+            <View>
+              <Text style={styles.mainText}>
+                {deck.questions[idx].question}
+              </Text>
+              <Text
+                style={styles.subText}
+                onPress={() => this.setState({ showAnswer: true })}
+              >
+                Show Answer
+              </Text>
+            </View>
+          )}
+
+          <View style={{ marginTop: 16 }}>
+            <Button
+              onPress={this._handleCorrectPress.bind(this)}
+              title="Correct"
+              color="#841584"
+            />
+          </View>
+
+          <View style={{ marginTop: 16 }}>
+            <Button
+              onPress={this._handleIncorrectPress.bind(this)}
+              title="Incorrect"
+              color="#841584"
+            />
+          </View>
+        </ScrollView>
+      );
+    } else {
+      return <Text>No Questions For This Quiz</Text>;
+    }
+  }
+
+  _handleTryAgainPress() {
+    this.setState({
+      idx: 0,
+      correct: 0,
+      showAnswer: false,
+      finished: false
+    });
+  }
+
+  _handleHomePress() {
+    this.setState({
+      idx: 0,
+      correct: 0,
+      showAnswer: false,
+      finished: false
+    });
+    this.props.navigation.navigate("Home");
+  }
+
+  _handleCorrectPress() {
+    const update =
+      this.state.idx === this.state.deck.questions.length - 1
+        ? {
+            finished: true,
+            correct: this.state.correct + 1
+          }
+        : {
+            correct: this.state.correct + 1,
+            idx: this.state.idx + 1
+          };
+    this.setState(update);
+  }
+
+  _handleIncorrectPress() {
+    const update =
+      this.state.idx === this.state.deck.questions.length - 1
+        ? { finished: true }
+        : { idx: this.state.idx + 1 };
+    this.setState(update);
   }
 }
 
@@ -28,5 +167,16 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     backgroundColor: "#fff"
+  },
+  mainText: {
+    fontSize: 28,
+    textAlign: "center",
+    marginTop: 16,
+    marginBottom: 8
+  },
+  subText: {
+    fontSize: 18,
+    textAlign: "center",
+    color: "#841584"
   }
 });
